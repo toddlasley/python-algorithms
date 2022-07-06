3d798c612d9a61fb72b4b77def3f981a3ef00b09
toddlasley <todd.r.lasley@gmail.com>
2022-07-06 12:03:29 -0700

    adding another test for rotate image problem

--- tests/leet_code/medium/test_rotate_image.py
+++ tests/leet_code/medium/test_rotate_image.py
@@ -10,3 +10,8 @@ def test_rotate_image():
     b_expected = [[6, 3, 0], [7, 4, 1], [8, 5, 2]]
     rotate_image(b)
     assert b == b_expected
+
+    c = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+    c_expected = [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+    rotate_image(c)
+    assert c == c_expected
